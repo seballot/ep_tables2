0242be3486bcd71ead639d982a2dc58c2666d8e0
ilmar <ilmar.tyrk@gmail.com>
2018-04-27 11:55:09 +0300

    html export fix

--- export2html.js
+++ export2html.js
@@ -2,12 +2,12 @@
 var DatatablesRendererExport = require('ep_tables2/static/js/datatables-renderer.js');
 
 exports.getLineHTMLForExport = function (hook, context) {
-  console.debug('EP_TABLES2 PRE', context.lineContent);
   if (context.text.indexOf("data-tables") != -1) {
     var attribIndex = retrieveIndex(context.attribLine);
-    var dtAttrs = context.apool.numToAttrib[attribIndex][1];
-    context.lineContent = DatatablesRendererExport.DatatablesRenderer.render("export", context, dtAttrs);
-    console.debug('EP_TABLES2 POST', context.lineContent);
+    if (attribIndex) {
+      var dtAttrs = context.apool.numToAttrib[attribIndex][1];
+      context.lineContent = DatatablesRendererExport.DatatablesRenderer.render("export", context, dtAttrs);
+    }
   }
   return true;
 };
@@ -15,7 +15,6 @@ exports.getLineHTMLForExport = function (hook, context) {
 retrieveIndex = function (attribLine) {
   var arr_indices = {0 : 0, 1 : 1, 2 : 2, 3 : 3, 4 : 4, 5 : 5, 6 : 6, 7 : 7, 8 : 8, 9 : 9, 'a' : 10, 'b' : 11, 'c' : 12, 'd' : 13, 'e' : 14, 'f' : 15, 'g' : 16, 'h' : 17, 'i' : 18, 'j' : 19, 'k' : 20, 'l' : 21, 'm' : 22, 'n' : 23, 'o' : 24, 'p' : 25, 'q' : 26, 'r' : 27, 's' : 28, 't' : 29, 'u' : 30, 'v' : 31, 'w' : 32, 'x' : 33, 'y' : 34, 'z' : 35};
   var attribIndex = 0;
-  console.log(attribLine);
   attribLine = attribLine.split("*");
   var index = 1;
   if (attribLine.length >= 3) {
