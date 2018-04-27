146105b94be816419f1923559868e0777a5c1951
ilmar <ilmar.tyrk@gmail.com>
2018-04-27 11:53:45 +0300

    html export fix

--- export2html.js
+++ export2html.js
@@ -2,19 +2,26 @@
 var DatatablesRendererExport = require('ep_tables2/static/js/datatables-renderer.js');
 
 exports.getLineHTMLForExport = function (hook, context) {
+  console.debug('EP_TABLES2 PRE', context.lineContent);
   if (context.text.indexOf("data-tables") != -1) {
     var attribIndex = retrieveIndex(context.attribLine);
     var dtAttrs = context.apool.numToAttrib[attribIndex][1];
-    return DatatablesRendererExport.DatatablesRenderer.render("export", context, dtAttrs);
+    context.lineContent = DatatablesRendererExport.DatatablesRenderer.render("export", context, dtAttrs);
+    console.debug('EP_TABLES2 POST', context.lineContent);
   }
+  return true;
 };
 
 retrieveIndex = function (attribLine) {
   var arr_indices = {0 : 0, 1 : 1, 2 : 2, 3 : 3, 4 : 4, 5 : 5, 6 : 6, 7 : 7, 8 : 8, 9 : 9, 'a' : 10, 'b' : 11, 'c' : 12, 'd' : 13, 'e' : 14, 'f' : 15, 'g' : 16, 'h' : 17, 'i' : 18, 'j' : 19, 'k' : 20, 'l' : 21, 'm' : 22, 'n' : 23, 'o' : 24, 'p' : 25, 'q' : 26, 'r' : 27, 's' : 28, 't' : 29, 'u' : 30, 'v' : 31, 'w' : 32, 'x' : 33, 'y' : 34, 'z' : 35};
   var attribIndex = 0;
-
+  console.log(attribLine);
   attribLine = attribLine.split("*");
-  attribLine = attribLine[2].split('+');
+  var index = 1;
+  if (attribLine.length >= 3) {
+    index = 2;
+  }
+  attribLine = attribLine[index].split('+');
   attribLine = attribLine[0] + "";
 
   for (var i = 0; i < attribLine.length; i++) {
