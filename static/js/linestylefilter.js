421c5a4a47a1c5388de4a106678ea4e4883db762
Sebastian Castro <90scastro@gmail.com>
2018-09-27 12:47:27 +0200

    Enable author color

--- static/js/linestylefilter.js
+++ static/js/linestylefilter.js
@@ -1,8 +1,9 @@
 exports.disableAuthorColorsForThisLine = function (hook, context) {
-    var lineText = context.text;
-    var disableLineColors = false;
-    if (lineText && lineText.indexOf('data-tables')!=-1) {
-       disableLineColors = true;
-    }
-    return disableLineColors;
+    return false;
+    // var lineText = context.text;
+    // var disableLineColors = false;
+    // if (lineText && lineText.indexOf('data-tables')!=-1) {
+    //    disableLineColors = true;
+    // }
+    // return disableLineColors;
 };
